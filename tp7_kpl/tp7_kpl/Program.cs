5b2943f5cd85e806860a0f699bbd65de3e57d1aa
reyhanhaidar <azanireyhan@student.telkomuniversity.ac.id>
2023-04-02 22:42:11 +0700

    menambahkan mahasiswa_1302210110

---
+++ tp7_kpl/tp7_kpl/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tp7_kpl;
+public class Program
+{
+    public static void Main()
+    {
+        Mahasiswa1302210110.ReadJSON();
+    }
+}
