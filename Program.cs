d3abaf2a57e986e932c47325c2916ae995a3662c
andryfall <andryfall@student.telkomuniversity.ac.id>
2023-04-02 17:02:58 +0700

    menambahkan class KuliahMahasiswa1302213005

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using tpmodul7_kelompok_2;
-
 public class Program
 {
     public static void Main()
     {
         DataMahasiswa1302213005.ReadJSON();
+
+        KuliahMahasiswa1302213005.ReadJSON();
     }
 }
