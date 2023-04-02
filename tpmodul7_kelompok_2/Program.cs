69342aa6bfda808b659b13af981206bf0bd98a71
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-04-02 21:39:35 +0700

    membuat main

--- tpmodul7_kelompok_2/Program.cs
+++ tpmodul7_kelompok_2/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul7_kelompok_2;
+public class Program
+{
+    public static void Main()
+    {
+        DataMahasiswa1302210123.ReadJSON();
+
+        KuliahMahasiswa1302210123.ReadJSON();
+    }
+}
