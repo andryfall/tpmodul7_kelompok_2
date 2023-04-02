5ec724cd2d0d5f42faa23d81e903bf603a7f4da6
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-04-02 21:35:20 +0700

    menambahkan class DataMahasiswa1302210123

---
+++ tpmodul7_kelompok_2/KuliahMahasiswa1302210123.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_2
+{
+    internal class KuliahMahasiswa1302210123
+    {
+       
+    }
+}
